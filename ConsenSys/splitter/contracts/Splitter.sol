1feb0e94a5deed10422fb5fd7dff0886edf8d5f0
Nikola <nikola.pavlov1955@gmail.com>
2018-03-27 11:44:48 +0300

    splitter init

---
+++ ConsenSys/splitter/contracts/Splitter.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.0;
+
+
+contract Splitter {
+    address public owner;
+    mapping (address => uint) public balances;
+
+
+    // events
+    event log_split(address depostitor, address out1, address out2, uint amount);
+
+    modifier only_owner() {
+        require (msg.sender == owner);
+        _;
+    }
+
+    // constructor
+    function Splitter () {
+        owner == msg.sender;
+    }
+
+    function split(address out1, address out2) payable returns (bool success) {
+        require(msg.value > 0);
+        require(out1 != 0);
+        require(out2 != 0);
+
+        if (msg.value % 2 == 1) {
+            balances[msg.sender] += 1;
+        }
+
+        balances[out1] += msg.value / 2;
+        balances[out2] += msg.value / 2;
+
+        log_split(msg.sender, out1, out2, msg.value);
+        return true;
+    }
+
+    function checkBalance(address adrr) returns (uint) {
+        return address(adrr).balance;
+    }
+
+    function killTheContract() returns (bool success) {
+        suicide(owner);
+        return true;
+    }
+}
