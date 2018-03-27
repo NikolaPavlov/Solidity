70828bc95dc751c39e2865c857f739e0ad7a5238
Nikola <nikola.pavlov1955@gmail.com>
2018-03-27 21:02:01 +0300

    quickfix, TODO: fix fee, TODO: refund

--- ConsenSys/remittance/contracts/Remittance.sol
+++ ConsenSys/remittance/contracts/Remittance.sol
@@ -6,7 +6,7 @@ contract Remittance {
     uint public FEE = 1;
     uint public DURATION = 1000;
 
-    mapping(bytes32 => Transfer) private transfers;
+    mapping(bytes32 => Transfer) private pending_transfers;
 
     struct Transfer {
         address creator;
@@ -15,9 +15,8 @@ contract Remittance {
         uint deadline;
     }
 
-    // logs
-    event LogHashedPasses(bytes32 passhash);
-    event LogCreateTransfer(address creator, address recipient, uint value, string pass1, string pass2);
+    event LogCreateTransfer(address creator, address recipient, uint value, bytes32 passhash);
+    event LogWithdrawTransfer(address recipient, uint value, bytes32 passhash);
 
 
     function Remittance() {
@@ -26,26 +25,36 @@ contract Remittance {
 
     function createTransfer(address transferRecipient, string pass1, string pass2) public payable returns (bool success) {
         require(msg.value > FEE);
+        require(transferRecipient != msg.sender);
+
         Transfer memory newTransfer;
         newTransfer.creator = msg.sender;
         newTransfer.recipient = transferRecipient;
         newTransfer.amount = msg.value - FEE;
         newTransfer.deadline = block.number + DURATION;
-        transfers[keccak256(pass1, pass2)] = newTransfer;
-        LogCreateTransfer(newTransfer.creator, newTransfer.recipient, newTransfer.amount, pass1, pass2);
-        LogHashedPasses(keccak256(pass1, pass2));
+        bytes32 passhash = keccak256(pass1, pass2);
+        pending_transfers[passhash] = newTransfer;
+
+        LogCreateTransfer(newTransfer.creator, newTransfer.recipient, newTransfer.amount, passhash);
 
         return true;
     }
 
     function withdrawFunds(string pass1, string pass2) public returns (bool success) {
         bytes32 passhash = keccak256(pass1, pass2);
-        Transfer memory toWithdraw = transfers[passhash];
+
+        Transfer memory toWithdraw = pending_transfers[passhash];
+
         require(toWithdraw.amount > 0);
         require(msg.sender == toWithdraw.recipient);
         require(block.number <= toWithdraw.deadline);
-        delete transfers[passhash];
+
+        delete pending_transfers[passhash];
+
         toWithdraw.recipient.transfer(toWithdraw.amount);
+
+        LogWithdrawTransfer(msg.sender, toWithdraw.amount, passhash);
+
         return true;
     }
 }
