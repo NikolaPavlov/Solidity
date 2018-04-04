b0412cadb8f081c343190bdddb8b06490ed35127
Nikola <nikola.pavlov1955@gmail.com>
2018-04-04 22:46:01 +0300

    working on split test

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -2,8 +2,7 @@ pragma solidity ^0.4.0;
 
 contract Splitter {
     address public owner;
-    mapping (address => uint) public balances;
-
+    // mapping (address => uint) public balances;
 
     event log_split(address depostitor, address out1, address out2, uint amount);
 
@@ -12,34 +11,13 @@ contract Splitter {
         _;
     }
 
-    function () payable { }
-
-    function Splitter () {
+    function Splitter () public {
         owner == msg.sender;
     }
 
-    function mockingSplit(address out1, address out2) payable returns (bool success) {
-        // mocking of transfers in balances mapping
-        require(msg.value > 0);
-        require(out1 != 0);
-        require(out2 != 0);
-        require(msg.sender != out1);
-        require(msg.sender != out2);
-        require(out1 != out2);
+    function () payable { }
 
-        if (msg.value % 2 == 1) {
-            balances[msg.sender] += 1;
-        }
-
-        balances[out1] += msg.value / 2;
-        balances[out2] += msg.value / 2;
-
-        log_split(msg.sender, out1, out2, msg.value);
-
-        return true;
-    }
-
-    function realSplit(address out1, address out2) payable returns (bool success) {
+    function split(address out1, address out2) public payable returns (bool success) {
         // real transfers on the network
         require(msg.value > 0);
         require(out1 != 0);
@@ -60,15 +38,16 @@ contract Splitter {
         return true;
     }
 
-    function checkBalanceOf(address addr) returns (uint) {
+    function checkBalanceOf(address addr) public returns (uint) {
         return addr.balance;
     }
 
-    function checkContractBalance() returns (uint) {
+    function checkContractBalance() public returns (uint) {
         return this.balance;
     }
 
-    function killTheContract() onlyOwner {
-        suicide(owner);
+    function killTheContract() onlyOwner public {
+        // suicide(owner);
+        selfdestruct(owner);
     }
 }
--- splitter/test/TestSplitter.js
+++ splitter/test/TestSplitter.js
@@ -9,18 +9,56 @@ var Splitter = artifacts.require('./contracts/Splitter.sol');
 // })
 
 
-contract('Splitter', function (accounts) {
+contract('Splitter', function(accounts) {
+
+    ownerAcc = accounts[0];
+    acc1 = accounts[1];
+    acc2 = accounts[2];
+    // result = web3.eth.getBalance(acc1);
+    // console.log(result.plus(9).toString());
+    // console.log()
+
+    // beforeEach('setup contract for each test', async function() {
+        //pass
+    // });
 
     // starting balance of the contract should be 0
-    it('test1-starting balance of the contract 0', function() {
-        Splitter.deployed().then(function(instance){
-            return instance.checkContractBalance.call();
-        }).then(function(balance){
-            assert.equal(balance.toNumber(), 1, 'Starting balance should be 0');
-        });
-    });
+    // it('test1-starting balance of the contract should be 0', function() {
+    //     Splitter.deployed().then(function(instance) {
+    //         return instance.checkContractBalance.call();
+    //     }).then(function(balance){
+    //         assert.equal(balance.valueOf(), 0, 'Starting balance should be 0');
+    //     });
+    // });
 
     // successfully split transaction with even amount
+    it('test2-split deposit with even amount', function() {
+        var acc1StartBalance = accounts[1];
+        var acc2StartBalance = accounts[2];
+        var acc1EndBalance = accounts[1];
+        var acc2EndBalance = accounts[2];
+        amount = 10000000000;
+
+        return Splitter.deployed().then(function(instance) {
+            meta = instance;
+            return meta.checkBalanceOf.call(accounts[1]);
+        }).then(function(balance) {
+            // console.log(balance.toNumber());
+            acc1StartBalance = balance.toNumber();
+            return meta.split(acc1, acc2, {from: ownerAcc, value: amount})
+        }).then(function(result) {
+            // expect(result).to.be.true;
+            return meta.checkBalanceOf.call(accounts[1]);
+        }).then(function(balance) {
+            acc1EndBalance = balance.toNumber();
+            // console.log('final balance:' + balance.toString())
+            assert.equal(acc1StartBalance + (amount / 2), acc1EndBalance, 'nop');
+            console.log('1:' + acc1StartBalance + (amount / 2));
+            console.log('2:' + acc1EndBalance);
+        })
+    });
+
+
     // successfully split transaction with odd amount, 1 return to sender
     // can't perform split with 0 value transaction
     // can't perform split with one missing address
@@ -28,4 +66,4 @@ contract('Splitter', function (accounts) {
     // can't perform split if one of the output accounts is the sender account
     // only owner can kill the contract
     // other users can't kill the contract
-// })
+})
