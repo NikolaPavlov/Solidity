cbaaf39b840c1d967e2fb5e39dd1e764cb089a2e
Nikola <nikola.pavlov1955@gmail.com>
2018-04-06 18:21:09 +0300

    fix splitter tests to async await Js brain wash

--- splitter/test/TestSplitter.js
+++ splitter/test/TestSplitter.js
@@ -1,69 +1,109 @@
+var Splitter = artifacts.require("./Splitter.sol");
 var assert = require('chai').assert;
-var Splitter = artifacts.require('./contracts/Splitter.sol');
 
 
-// describe('Testing Outter desc', function() {
-//     it('testing forTesting', function() {
-//         assert.equal(Splitter.forTesting(), 'gogo');
-//     })
-// })
+contract('Splitter async', accounts => {
+    let splitter;
+
+    beforeEach('should deploy a new Splitter contract', async () => {
+        splitter = await Splitter.new({ from: accounts[0] })
+    })
 
 
-contract('Splitter', function(accounts) {
-
-    ownerAcc = accounts[0];
-    acc1 = accounts[1];
-    acc2 = accounts[2];
-    // result = web3.eth.getBalance(acc1);
-    // console.log(result.plus(9).toString());
-    // console.log()
-
-    // beforeEach('setup contract for each test', async function() {
-        //pass
-    // });
-
-    // starting balance of the contract should be 0
-    // it('test1-starting balance of the contract should be 0', function() {
-    //     Splitter.deployed().then(function(instance) {
-    //         return instance.checkContractBalance.call();
-    //     }).then(function(balance){
-    //         assert.equal(balance.valueOf(), 0, 'Starting balance should be 0');
-    //     });
-    // });
-
-    // successfully split transaction with even amount
-    it('test2-split deposit with even amount', function() {
-        var acc1StartBalance = accounts[1];
-        var acc2StartBalance = accounts[2];
-        var acc1EndBalance = accounts[1];
-        var acc2EndBalance = accounts[2];
-        amount = 10000000000;
-
-        return Splitter.deployed().then(function(instance) {
-            meta = instance;
-            return meta.checkBalanceOf.call(accounts[1]);
-        }).then(function(balance) {
-            // console.log(balance.toNumber());
-            acc1StartBalance = balance.toNumber();
-            return meta.split(acc1, acc2, {from: ownerAcc, value: amount})
-        }).then(function(result) {
-            // expect(result).to.be.true;
-            return meta.checkBalanceOf.call(accounts[1]);
-        }).then(function(balance) {
-            acc1EndBalance = balance.toNumber();
-            // console.log('final balance:' + balance.toString())
-            assert.equal(acc1StartBalance + (amount / 2), acc1EndBalance, 'nop');
-            console.log('1:' + acc1StartBalance + (amount / 2));
-            console.log('2:' + acc1EndBalance);
-        })
+    it('starting balance of the contract should be 0', async () => {
+        let balance = await splitter.checkContractBalance();
+        assert.equal(balance.toString(), 0, 'starting balance is not 0');
     });
 
 
-    // successfully split transaction with odd amount, 1 return to sender
-    // can't perform split with 0 value transaction
+    it('successfully split transaction with even incoming amount', async () => {
+        let value = 10000 // 10000 wei
+        let expected1Value = 100000000000000005000 // 100 Eth + 5000 wei
+        let expected2Value = 100000000000000005000 // 100 Eth + 5000 wei
+
+        let tx = await splitter.split(accounts[1], accounts[2], { from: accounts[0], value: value })
+        let account1Funds = await splitter.checkBalanceOf(accounts[1])
+        let account2Funds = await splitter.checkBalanceOf(accounts[2])
+
+        assert.equal(account1Funds.toNumber(), expected1Value);
+        assert.equal(account2Funds.toNumber(), expected2Value);
+    })
+
+
+    // need to check for 1 wei return to sender
+    // or change the split function to give one of the two addresses 1wei more on split
+    it('successfully split transaction with odd incoming amount', async () => {
+        let value = 10001 // 10000 wei
+        let expected1Value = 100000000000000010000
+        let expected2Value = 100000000000000010000 // 100 Eth + 5000 wei + 5000 wei
+
+        let tx = await splitter.split(accounts[1], accounts[2], { from: accounts[0], value: value })
+        let account1Funds = await splitter.checkBalanceOf(accounts[1])
+        let account2Funds = await splitter.checkBalanceOf(accounts[2])
+
+        assert.equal(account1Funds.toNumber(), expected1Value);
+        assert.equal(account2Funds.toNumber(), expected2Value);
+    })
+
+
+    it('should not allow split with 0 value in the transaction', async () => {
+        try {
+            let res = await splitter.split(accounts[1], accounts[2], { value: 0 });
+            assert.fail(res, true, 'transaction with 0 value passed');
+        }
+        catch(err) {
+            assert.include(err.message, 'revert');
+        }
+    })
+
+
+    it('should not allow split with empty value in the transaction', async () => {
+        try {
+            let res = await splitter.split(accounts[1], accounts[2]);
+            assert.fail(res, true, 'transaction with empty value passed');
+        }
+        catch(err) {
+            assert.include(err.message, 'revert');
+        }
+    })
+
+
+    it('should not allow split with sender account is one of the output accounts', async () => {
+        try {
+            let res = await splitter.split(accounts[0], accounts[1], {value: 10});
+            assert.fail(res, true, 'transaction with sender is one of the ouput accounts passed');
+        }
+        catch(err) {
+            assert.include(err.message, 'revert');
+        }
+    })
+
+
+    it('owner can kill the contract', async () => {
+        let result = await splitter.killTheContract();
+        // check if LogSelfDestruct event is fire before selfdestruct()
+        assert.equal(result.logs[0].event, 'LogSelfDestruct');
+    });
+
+
+    it('not owner can\'t kill the contract', async () => {
+        try {
+            await splitter.killTheContract({ from: accounts[3] });
+        }
+        catch(err) {
+            assert.include(err.message, 'revert');
+        }
+    });
+
+
+    // TODO:
+        // successfully split transaction with odd amount, 1 return to sender
+        // can't perform split with 0 value transaction
+        // can't perform split with empty value in transaction
     // can't perform split with one missing address
     // can't perform split with two missing address
-    // can't perform split if one of the output accounts is the sender account
-    // only owner can kill the contract
-    // other users can't kill the contract
+        // can't perform split if one of the output accounts is the sender account
+        // only owner can kill the contract
+        // other users can't kill the contract
+
 })
