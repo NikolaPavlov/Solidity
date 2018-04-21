7675ff966e9f0fc8a04701e3249af8fd4b9fd34e
Nikola <nikola.pavlov1955@gmail.com>
2018-04-21 08:52:47 +0300

    added tests to ico

--- ico_task/test/TestLimeCoinCrowdsale.js
+++ ico_task/test/TestLimeCoinCrowdsale.js
@@ -11,7 +11,6 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-// const LimcCoin = artifacts.require('./LimeCoin.sol');
 const LimeCoin = artifacts.require('./LimeCoin.sol');
 const LimeCoinCrowdsale = artifacts.require('./LimeCoinCrowdsale.sol');
 
@@ -21,13 +20,15 @@ contract('Test LimeCoinCrowdsale', accounts => {
     const rate = new BigNumber(100);
     const value = ether(11);
     const expectedTokenAmount = rate.mul(value);
-    const wallet = accounts[1]; // piggy bank acc ?
+    const wallet = accounts[1]; // piggy bank acc
     const purchaser = accounts[2];
     const investor = accounts[3];
+
 
     before(async function() {
         await advanceBlock();
     });
+
 
     beforeEach('beforeEach', async function () {
         this.openingTime = latestTime() + duration.weeks(1);
@@ -40,121 +41,113 @@ contract('Test LimeCoinCrowdsale', accounts => {
         await this.limeCoin.transferOwnership(this.limeCoinCrowdsale.address);
     });
 
-    // // it('should accept contribution to buyTokens function', async function () {
-    // //     let result = await this.limeCoinCrowdsale.buyTokens.call(this.investor, { value: 1000, from: purchaser });
-    // //     console.log(result);
-    // // });
-
-
-    // it('should not immediately assign tokens to beneficiary', async function () {
-    //     await increaseTimeTo(this.openingTime);
-    //     await this.limeCoinCrowdsale.buyTokens(this.investor, { value: value, from: purchaser });
-    //     const balance = await this.token.balanceOf(this.investor);
-    //     balance.should.be.bignumber.equal(0);
-    // });
-
-
-    it('should not allow beneficiaries to withdraw tokens before crowdsale ends', async function () {
-        await increaseTimeTo(this.beforeEndTime);
-        await this.limeCoinCrowdsale.buyTokens.call(investor, { value: value, from: purchaser }).should.be.fulfilled;
-        await this.limeCoinCrowdsale.withdrawTokens.call({ from: investor }).should.be.rejectedWith(EVMRevert);
-    });
-
-
-    // TODO: fix
-    // it('should allow beneficiaries to withdraw tokens after crowdsale ends', async function () {
-    //     await increaseTimeTo(this.openingTime);
-    //     console.log(latestTime());
-    //     let r1 = await this.limeCoinCrowdsale.buyTokens.call(investor, { value: value, from: purchaser });
-    //     console.log(r1);
-    //     await increaseTimeTo(this.afterClosingTime);
-    //     console.log(latestTime());
-        // let r2 = await this.limeCoinCrowdsale.withdrawTokens.call({ from: investor });
-        // console.log(r2);
-    // });
-
 
     describe('accepting payments', function () {
-        it('should accept payments', async function () {
-            // await this.limeCoinCrowdsale.send(value).should.be.fulfilled;
+        it('can buy tokens when crowdsale is open', async function () {
             await increaseTimeTo(this.beforeEndTime);
             await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser }).should.be.fulfilled;
+        });
+
+
+        it('can\'t buy tokens when crowdsale is closed', async function () {
+            await increaseTimeTo(this.afterClosingTime);
+            await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser }).should.be.rejectedWith(EVMRevert);
+        });
+
+
+        it('should forward funds to piggy bank wallet', async function () {
+            const pre = web3.eth.getBalance(wallet);
+            await increaseTimeTo(this.beforeEndTime);
+            await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
+            const post = web3.eth.getBalance(wallet);
+            post.minus(pre).should.be.bignumber.equal(value);
+        });
+
+
+        it('correctly assign weiRaised amount', async function () {
+            await increaseTimeTo(this.openingTime + duration.days(15));
+            await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
+            let weiRaised = await this.limeCoinCrowdsale.weiRaised.call();
+            weiRaised.should.be.bignumber.equal(value);
         });
     });
 
 
-    // TODO: fix
-    // it('should assign tokens to beneficiary', async function () {
-    //     await increaseTimeTo(this.beforeEndTime);
-    //     await this.limeCoinCrowdsale.buyTokens.call(investor, { value: value, from: purchaser });
-    //     const balance = await this.limeCoin.balanceOf(investor);
-    //     console.log(balance.toString());
-        // balance.should.be.bignumber.equal(expectedTokenAmount);
-    // });
+    describe('tokens operations', function () {
+        it('should allow beneficiaries to withdraw tokens after crowdsale ends', async function () {
+            await increaseTimeTo(this.beforeEndTime);
+            await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
+            await increaseTimeTo(this.afterClosingTime);
+            await this.limeCoinCrowdsale.withdrawTokens.call({ from: investor }).should.be.fulfilled;
+        });
 
 
-    // TODO: fix
-    // it('should forward funds to wallet', async function () {
-    //   const pre = web3.eth.getBalance(wallet);
-    //   await this.limeCoinCrowdsale.buyTokens(investor, { value, from: purchaser });
-    //   const post = web3.eth.getBalance(wallet);
-    //   post.minus(pre).should.be.bignumber.equal(value);
-    // });
+        it('shouldn\'t allow beneficiaries to withdraw tokens before crowdsale ends', async function () {
+            await increaseTimeTo(this.beforeEndTime);
+            await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
+            await this.limeCoinCrowdsale.withdrawTokens.call({ from: investor }).should.be.rejectedWith(EVMRevert);
+        });
+    });
 
 
     describe('rate tests', function () {
         it('rate test 1st week', async function () {
             await increaseTimeTo(this.openingTime + duration.days(1));
             let rate = await this.limeCoinCrowdsale.getCurrentRate.call();
-            assert.equal(rate, 500, 'rate should be 500');
+            assert.equal(rate.toNumber(), 500, 'rate should be 500');
         });
+
 
         it('rate test 1st week with contributed more than 10Eth', async function () {
             await increaseTimeTo(this.openingTime + duration.days(1));
             await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
             let rate = await this.limeCoinCrowdsale.getCurrentRate.call();
-            assert.equal(rate, 300, 'rate should be 300');
+            assert.equal(rate.toNumber(), 300, 'rate should be 300');
         });
+
 
         it('rate test 2nd week', async function () {
-            await increaseTimeTo(this.openingTime + duration.weeks(2));
+            await increaseTimeTo(this.openingTime + duration.days(8));
             let rate = await this.limeCoinCrowdsale.getCurrentRate.call();
-            assert.equal(rate, 200, 'rate should be 200');
+            assert.equal(rate.toNumber(), 200, 'rate should be 200');
         });
+
 
         it('rate test 2nd week with contributed more than 30Eth', async function () {
-            await increaseTimeTo(this.openingTime + duration.weeks(2));
+            await increaseTimeTo(this.openingTime + duration.days(8));
             await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
             await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
             await this.limeCoinCrowdsale.buyTokens(investor, { value: value, from: purchaser });
             let rate = await this.limeCoinCrowdsale.getCurrentRate.call();
-            assert.equal(rate, 150, 'rate should be 150');
+            assert.equal(rate.toNumber(), 150, 'rate should be 150');
         });
 
+
         it('rate test 3rd week', async function () {
-            await increaseTimeTo(this.openingTime + duration.weeks(3));
+            await increaseTimeTo(this.openingTime + duration.days(15));
             let rate = await this.limeCoinCrowdsale.getCurrentRate.call();
-            assert.equal(rate, 100, 'rate should be 100');
+            assert.equal(rate.toNumber(), 100, 'rate should be 100');
         });
     });
 
 
     describe('mint tokens', function () {
-        it('Can mint tokens', async function () {
-            await increaseTimeTo(this.openingTime + duration.weeks(1));
-            let r1 = await this.limeCoinCrowdsale.mintTokensFor.call(investor, 100, { from: accounts[0] });
-            console.log(r1);
-        })
-    })
+        it('can mint tokens before crowdsale opening', async function () {
+            await increaseTimeTo(this.openingTime - duration.days(1));
+            await this.limeCoinCrowdsale.mintTokensFor.call(investor, 100, { from: purchaser }).should.be.fulfilled;
+        });
 
 
+        it('can mint tokens when crowdsale is open', async function () {
+            await increaseTimeTo(this.beforeEndTime);
+            await this.limeCoinCrowdsale.mintTokensFor.call(investor, 100, { from: purchaser }).should.be.fulfilled;
+        });
 
 
-
-    // TODO:
-    //     - mint tokens test
-    //     - cant buy tokens after end time
-    //     - has closed is true after endtime
-    //     - check correctnes of weiRaised
+        it('can mint tokens after the crowdsale is over', async function () {
+            await increaseTimeTo(this.afterClosingTime);
+            await this.limeCoinCrowdsale.mintTokensFor.call(investor, 100, { from: purchaser }).should.be.fulfilled;
+        });
+    });
 
 })
